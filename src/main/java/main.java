4c060c05730b5ef15921cee3b71c3b8da89365d6
Kim-KiBoum <rlarl7477@atlasnetworks.co.kr>
2023-12-05 16:32:36 +0900

    [UPDATE] jar, logic change

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,9 @@
 public class main {
 
-  public static void main(String[] args) {
-    System.out.println("TESt");
+  public static void main(String[] args) throws InterruptedException {
+    while (true) {
+      Thread.sleep(5000);
+      System.out.println("APM TEST");
+    }
   }
 }
